75a918a5a53e62859e91a6a5490f147987d826a8
Thisen-Ekanayake <thisenekanayake0330@gmail.com>
2026-08-26 22:14:37 +0530

    Add Qdrant collection creation prototype

---
+++ prototype/create_collection.py
@@ -0,0 +1,15 @@
+from qdrant_client import QdrantClient
+from qdrant_client.models import Distance, VectorParams
+
+client = QdrantClient(
+    host="localhost",
+    port=6337,
+)
+
+client.create_collection(
+    collection_name="ceynex",
+    vectors_config=VectorParams(
+        size=384,
+        distance=Distance.COSINE,
+    )
+)
